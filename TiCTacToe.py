6623d5c297ab1080d19f28d303ad4e3a7d6f8bb2
ThibaultJOVELIN <74654940+ThibaultJOVELIN@users.noreply.github.com>
2020-11-18 12:30:30 +0100

    Update TiCTacToe.py

--- TiCTacToe.py
+++ TiCTacToe.py
@@ -4,7 +4,18 @@ def initooaliseGrille(grille):
         grille[compteur] = " "
     return grille
 
-def AfficheGrille(grille):
+def afficheGrille(grille):
     for i in range (0,3):
-        print(grille[3*i],grille[3*i+1],grille[3*i+2])
-    return grille
+        print(grille[3*i],grille[3*i+1],grille[3*i+2],'\n')
+    return grille   
+
+def ajouteSymbole(grille,joueur):
+    i = 0
+    j = 0
+    choixIncorrect = True
+    while choixIncorrect :
+        i = int(input("Sur quelle ligne voulez vous jouer ?"))
+        j = int(input("Sur quelle colonne voulez-vouz jouer ?"))
+        if (grille[3*i+j]!= " "
+            choixIncorrect = False
+    return grille 
