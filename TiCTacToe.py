60cdfbe2d5e826f2f64415ac306bd401469cbd2e
ThibaultJOVELIN <74654940+ThibaultJOVELIN@users.noreply.github.com>
2020-11-18 12:16:26 +0100

    Update TiCTacToe.py

--- TiCTacToe.py
+++ TiCTacToe.py
@@ -1,5 +1,10 @@
-def initooaliseGrille(grille)
+def initooaliseGrille(grille):
     compteur = 0
     for compteur in range (0,9):
         grille[compteur] = " "
+    return grille
+
+def AfficheGrille(grille):
+    for i in range (0,3):
+        print(grille[3*i],grille[3*i+1],grille[3*i+2])
     return grille
